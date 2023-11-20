0cd18653f16f93c5e07c7a636af2a33c94fcdbf8
Wesley <124817198+WexleySad@users.noreply.github.com>
2023-11-20 14:55:02 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,14 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo', 
+{
+    duration: 2000, 
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', 
+{
+    duration: 2000, 
+    distance: '90px'
+})
