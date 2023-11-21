c12a52ab53ff2c967703c527e06f0e0fd172a746
Wesley <124817198+WexleySad@users.noreply.github.com>
2023-11-21 11:00:27 -0300

    Add files via upload

--- reveal.js
+++ reveal.js
@@ -1,14 +1,206 @@
-window.revelar = ScrollReveal({reset:true})
-
+window.revelar = ScrollReveal({ reset: true })
+//INDEX.HTML
 // TOPO DO SITE
-revelar.reveal('.efeito-txt-topo', 
-{
-    duration: 2000, 
-    distance: '90px'
-})
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 2000,
+        distance: '90px'
+    })
 
-revelar.reveal('.efeito-img-topo', 
-{
-    duration: 2000, 
-    distance: '90px'
-})
+revelar.reveal('.efeito-img-topo',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+
+// OS TITULOS DA SESSOES
+revelar.reveal('.titulo-edit',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+
+//SESSAO DE ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1000
+    })
+
+revelar.reveal('.efeito-especialidades3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500
+    })
+
+revelar.reveal('.efeito-especialidades4',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2000
+    })
+
+revelar.reveal('.efeito-especialidades5',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2500
+    })
+
+revelar.reveal('.efeito-especialidades6',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 3000
+    })
+
+//EFEITO PORTIFOLIO
+revelar.reveal('.efeito-portifolio1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'bottom'
+    })
+
+revelar.reveal('.efeito-portifolio2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'top'
+    })
+
+revelar.reveal('.efeito-portifolio3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'bottom'
+    })
+
+// EFEITO FORMULARIO
+revelar.reveal('.efeito-formulario1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-formulario2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-formulario3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-formulario4',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-formulario-btn',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+
+
+//SERVIÇOS.HTML
+
+
+//COOPERADORES.HTML
+
+
+//SOBRE.HTML
+//EFEITO HISTORIA
+revelar.reveal('.efeito-historia-txt',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-historia-img',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+//EFEITO ORIENTACAO
+revelar.reveal('.efeito-orientacao1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1000,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao-txt1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-orientacao-txt2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1000,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-orientacao-txt3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500,
+        origin: 'right'
+    })
+
