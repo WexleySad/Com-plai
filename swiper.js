60a1b22ade6d231adc25c40f016b3027cf054050
Wesley <124817198+WexleySad@users.noreply.github.com>
2023-11-30 15:05:08 -0300

    Add files via upload

--- swiper.js
+++ swiper.js
@@ -1,12 +1,21 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+const swiper = new Swiper('.sample-slider', {
+    loop: true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 30,
+        depth: 200,
+        stretch: 30,
+        modifier: 1,
+        scale: 1,
+        slideShadows: false,
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
     },
-});
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
